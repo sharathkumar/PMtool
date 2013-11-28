3a623c89ef4a18c7ab50c7553843cdff132238a6
sharathkumar <sharathkumar@qburst.com>
2013-11-28 20:12:32 +0530

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PMtool::Application.config.secret_key_base = '39d5e5b8d2a110dff2cc47a616c604b5f8b74f464aa73bb9ffff49dce8ca021f3743b800e903a4a18c90562d2cb2d170a4a2dc40ec16a046505243c6dc850fbf'
